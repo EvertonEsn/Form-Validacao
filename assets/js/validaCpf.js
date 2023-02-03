dd140efef01329d7fea5167a893d3995d7e1ffef
¨EvertonEsn¨ <everton.esn10@gmail.com>
2023-02-03 19:42:53 -0300

    ¨first¨

---
+++ assets/js/validaCpf.js
@@ -0,0 +1,46 @@
+class ValidaCpf {
+  constructor(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+      writable: false,
+      value: cpfEnviado.replace(/\D+/g, '')
+    });
+  }
+
+  valida() {
+    if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.sequencia(this.cpfLimpo)) return false;
+
+    let cpfArray = Array.from(this.cpfLimpo);
+
+    cpfArray = cpfArray.slice(0, -2);
+
+    const digito1 = cpfArray.push(this.criaDigito(cpfArray));
+    // const digito2 = criaDigito(digito1, 2);
+    const digito2 = cpfArray.push(this.criaDigito(cpfArray));
+
+    return this.cpfLimpo === cpfArray.join('');
+  }
+
+  criaDigito(cpf) {
+    let regressivo = cpf.length + 1;
+
+    const calc = cpf.reduce((ac, val) => {
+      ac = ac + Number(val) * regressivo;
+      regressivo--;
+      return ac;
+    }, 0);
+
+    const digito = 11 - (calc % 11);
+
+    return digito > 9 ? 0 : digito;
+  }
+
+  sequencia(cpf) {
+    const cpfSequencia = cpf.repeat(cpf[0], 11);
+    return cpfSequencia === cpf;
+  }
+}
+
+// const cpf = new ValidaCpf('705.484.450-52');
+// // console.log(cpf.valida());
